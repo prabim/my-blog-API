0866e27722ff3e4c12c211ad8574bbd014595cd9
Prabim <prabeam@gmail.com>
2020-09-05 16:16:10 +0545

    First commit

---
+++ src/build/precache-manifest.2bb637be9e35275b8c7221643b9821ac.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6bd07a643d284eb9eaf9",
+    "url": "/static/css/main.4faccb8a.chunk.css"
+  },
+  {
+    "revision": "6bd07a643d284eb9eaf9",
+    "url": "/static/js/main.ef1b37c8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f68f7937a277a9d1be07",
+    "url": "/static/js/2.ed707dbc.chunk.js"
+  },
+  {
+    "revision": "91c2014b4edc8c13ef8399939e04f104",
+    "url": "/index.html"
+  }
+];
